e4aa55881e60466a605c66a3a64000879cbb0d7b
Shubham Vishwakarma <shubham.vishwakarma@toothsi.in>
2023-05-31 17:08:53 +0530

    minor changes

--- server/src/queries/migrations.js
+++ server/src/queries/migrations.js
@@ -2,13 +2,21 @@ const db = require('./db').getDb();
 const path = require('path');
 
 const migrateProperImages = () => {
-  const project24Images = db.prepare(`
+  const project24Images = db
+    .prepare(
+      `
   select * from Images where projectsId=24 and labeled=1 and labelData like '{"labels":{"afdmj2rxn":["PROPER"],"__temp":[]}%''
-  `);
+  `
+    )
+    .all();
   console.log(project24Images);
-  const project22Images = db.prepare(`
+  const project22Images = db
+    .prepare(
+      `
   select * from Images where projectsId=22 and labeled=1'
-  `);
+  `
+    )
+    .all();
   console.log(project22Images);
   for (let i = 0; i < 10; i++) {
     console.log(project22Images[i]);
