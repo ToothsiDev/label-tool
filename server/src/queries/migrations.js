0bd829d2c207d0d7558fee540c64679988435fa7
Shubham Vishwakarma <shubham.vishwakarma@toothsi.in>
2023-06-01 13:13:09 +0530

    minor changes

--- server/src/queries/migrations.js
+++ server/src/queries/migrations.js
@@ -70,4 +70,57 @@ const migrateProperImages = () => {
   console.log(gradeWiseBreakup);
 };
 
-migrateProperImages();
+const migrateProperImagesNew = () => {
+  const project10Images = db
+    .prepare(
+      `
+  select * from Images where projectsId=10 and labeled=1
+  `
+    )
+    .all();
+  const properImageIds = [];
+  const properImageObjs = {};
+  const gradeWiseBreakup = {};
+  for (let i = 0; i < project10Images.length; i++) {
+    const cur = project10Images[i];
+    const refImageObj = cur;
+    if (refImageObj) {
+      const refImageLabel = JSON.parse(refImageObj.labelData);
+      console.log(refImageLabel, refImageObj);
+      if (refImageLabel['labels']['afdmj2rxn'][0] == 'PROPER') {
+        properImageIds.push(refImageObj.id);
+        // db.prepare(
+        //   `update Images set projectsId=28 where projectsId=22 and id=?`
+        // ).run(refImageObj.id);
+        if (gradeWiseBreakup[refImageLabel['labels']['607jvc0vd'][0]]) {
+          gradeWiseBreakup[refImageLabel['labels']['607jvc0vd'][0]] += 1;
+        } else {
+          gradeWiseBreakup[refImageLabel['labels']['607jvc0vd'][0]] = 1;
+        }
+        properImageObjs[refImageObj.id] = {
+          '22': {
+            id: refImageObj.id,
+            oName: refImageObj.originalName,
+            labelData: JSON.parse(refImageObj.labelData),
+          },
+        };
+      }
+    }
+  }
+  // db.prepare(
+  //   `
+  //   update Images set projectsId=28 where projectsId=22 and id in (${properImageIds
+  //     .map(cur => '?')
+  //     .join(',')})
+  // `
+  // ).run(properImageIds);
+  console.log('properImageIds', properImageIds, properImageIds.length);
+  // fs.writeFile(
+  //   './output_migrations_final.json',
+  //   JSON.stringify(properImageObjs)
+  // );
+  console.log(gradeWiseBreakup);
+};
+
+// migrateProperImages();
+migrateProperImagesNew();
